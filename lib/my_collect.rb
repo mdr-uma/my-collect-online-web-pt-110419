a6211a3a4e7b16a01ef70d08816e8f980cb84f99
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-15 02:53:31 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(languages)
    i = 0
   collection = []
    while i < collection.length
-     yield my_collect[languages] {|language| language.upcase << collection}
+     yield my_collect[languages] {|language| << collection language.upcase }
          i += 1
      end
      collection

50005091a61c62249e7692f13c4dc20fd5840eed
Uma Manandhar <um_2003_8@hotmail.com>
2019-11-15 04:13:43 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(collection)
    i = 0
   result = []
    while i < collection.length
-     result << (yield array[i])
+     result << (yield collection[i])
          i += 1
      end
      result
